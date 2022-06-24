2f2d9a12fc4469780c4416b6f5b22f8573e2e59f
RJAttwell <richardjattwell@gmail.com>
2022-06-24 13:22:00 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -3,19 +3,23 @@ gamePattern = [];
 
 function nextSequence(){
 
-    var randomNum = Math.random();
-    randomNum = Math.floor((randomNum * 3)+1);
+    var randomNum = Math.floor(Math.random()* 4);
+
+    var randomChosenColour = buttonColours[randomNum];
+    gamePattern.push(randomChosenColour);
+
+    //Select button with same ID as the random chosen colour
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    //Play a mp3 file associated with that colour from sounds folder
+    var randomSound = new Audio("sounds/" + randomChosenColour + ".mp3");
+    randomSound.play(); 
 
 };
 
-var randomChosenColour = buttonColours[randomNum];
-gamePattern.push(randomChosenColour);
 
-$("button").on("click", function(){
-    $("button").animate(
-        {
-            opacity: 0.5,
-            margin: "10%"
-        }
-    );
-});
+$("keydown", function(event){
+
+
+    
+})
